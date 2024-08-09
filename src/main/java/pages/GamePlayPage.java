fe8d625952d1e04b6250eace391b7d14ce1e4aac
SiminaRe <simina.rendler@altom.com>
2024-08-09 16:27:24 +0300

    add testCollectFishBonesOnMiddleLane
    Add a test that compares the number of coins displayed at the end against the number of fishbones the cat collect from the middle lane. This is a "good-enough" test that covers a good amount of cases. It doesn't cover the cases where the cat reaches 150m+, when origin resets occurs for most fishbones; this needs to be handled

--- src/main/java/pages/GamePlayPage.java
+++ src/main/java/pages/GamePlayPage.java
@@ -1,8 +1,6 @@
 package pages;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import com.alttester.AltDriver;
 import com.alttester.AltObject;
@@ -11,7 +9,6 @@ import com.alttester.Commands.FindObject.AltWaitForObjectsParams;
 import com.alttester.Commands.ObjectCommand.AltCallComponentMethodParams;
 
 import static com.alttester.Commands.FindObject.AltFindObjectsParams.*;
-import static org.apache.logging.log4j.core.util.Integers.parseInt;
 
 public class GamePlayPage extends BasePage {
 
@@ -65,22 +62,17 @@ public class GamePlayPage extends BasePage {
         boolean movedRight = false;
 
         for (int i = 0; i < nrOfObstacles; i++) {
-            System.out.println("i= " + i);
 
             AltFindObjectsParams params = new AltFindObjectsParams.Builder(AltDriver.By.NAME, "Obstacle").build();
             List<AltObject> allObstacles = new ArrayList<>(Arrays.asList(getDriver().findObjectsWhichContain(params)));
-            System.out.println("   Toate obstacolele sunt in nr de " + allObstacles.size());
-            for (AltObject obs : allObstacles) {
-                System.out.println("   Obstacole inainte de filtrare: " + obs.name + ", z: " + obs.worldZ + ", x: " + obs.worldX);
-            }
 
-            // Sortare folosind expresii lambda
+
             allObstacles.sort((x, y) -> {
                 if (x.worldZ == y.worldZ) return 0;
                 return x.worldZ > y.worldZ ? 1 : -1;
             });
 
-            // Eliminarea obstacolelor care se află în spatele personajului
+            //remove obstacles behind the character
             List<AltObject> toBeRemoved = new ArrayList<>();
             for (AltObject obs : allObstacles) {
                 if (obs.worldZ < character1.worldZ)
@@ -88,17 +80,8 @@ public class GamePlayPage extends BasePage {
             }
             allObstacles.removeAll(toBeRemoved);
 
-            // Afișarea obstacolelor după filtrare
-            System.out.println("   Obstacole rămase după filtrare: " + allObstacles.size());
-            for (AltObject obs : allObstacles) {
-                System.out.println("    Obstacolele rămase după filtrare: " + obs.name + ", z: " + obs.worldZ + ", x: " + obs.worldX);
-            }
-
-            // Selectarea obstacolului cel mai apropiat
             AltObject obstacle = allObstacles.get(0);
-            System.out.println("   Closest obstacle: " + obstacle.name + ", z:" + obstacle.worldZ + ", x:" + obstacle.worldX);
 
-            // Bucla de așteptare cu limitare de timp
             long startTime = System.currentTimeMillis();
             while (obstacle.worldZ - character1.worldZ > 5 && (System.currentTimeMillis() - startTime) < 15000) {
                 params = new AltFindObjectsParams.Builder(AltDriver.By.ID, "" + obstacle.id).build();
@@ -110,65 +93,47 @@ public class GamePlayPage extends BasePage {
                 params = new AltFindObjectsParams.Builder(AltDriver.By.NAME, "PlayerPivot").build();
                 character1 = getDriver().findObject(params);
                 if (character1 == null) {
-                    throw new NullPointerException("PlayerPivot nu a fost găsit.");
-                }
-                System.out.println("   Actualizare: Character z:" + character1.worldZ + ", Obstacle " + obstacle.name + " z:" + obstacle.worldZ);
-            }
-            if (character1 == null || obstacle == null) {
-                System.out.println("Character or obstacle is null, skipping this iteration.");
-                continue;
-            }
-            // Acțiuni pentru evitare obstacole
-            if (obstacle.name.contains("ObstacleHighBarrier")) {
-                character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "Slide", "Assembly-CSharp", new Object[]{}).build(), Void.class);
-                System.out.println("    Slide pentru obstacol înalt");
-            } else if (obstacle.name.contains("ObstacleLowBarrier") || obstacle.name.contains("Rat")) {
-                character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "Jump", "Assembly-CSharp", new Object[]{}).build(), Void.class);
-                System.out.println("    Saritura pentru obstacol jos sau șobolan");
-            } else {
-                if (allObstacles.size() > 1 && obstacle.worldZ == allObstacles.get(1).worldZ) {
-                    System.out.println("   Două obstacole la aceeași distanță Z: " + obstacle.name + " și " + allObstacles.get(1).name);
-                    if (obstacle.worldX == character1.worldX) {
-                        // Primul obstacol este pe aceeași bandă ca și caracterul
-                        if (allObstacles.get(1).worldX == -1.5f) {
-                            // Al doilea obstacol este pe banda stângă
-                            character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{1}).build(), Void.class);
-                            movedRight = true;
-                            System.out.println("    Schimbare pe banda dreaptă");
-                        } else {
-                            // Al doilea obstacol este pe banda dreaptă sau centrală
-                            character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{-1}).build(), Void.class);
-                            movedLeft = true;
-                            System.out.println("    Schimbare pe banda stângă");
-                        }
-                    } else {
-                        if (allObstacles.get(1).worldX == character1.worldX) {
-                            // Al doilea obstacol este pe aceeași bandă ca și caracterul
-                            if (obstacle.worldX == -1.5f) {
-                                // Primul obstacol este pe banda stângă
-                                character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{1}).build(), Void.class);
-                                movedRight = true;
-                                System.out.println("   Schimbare pe banda dreaptă");
-                            } else {
-                                // Primul obstacol este pe banda dreaptă sau centrală
-                                character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{-1}).build(), Void.class);
-                                movedLeft = true;
-                                System.out.println("    Schimbare pe banda stângă");
-                            }
-                        }
-                    }
-                } else {
-                    // Dacă obstacolul curent este pe aceeași bandă ca și caracterul
-                    if (obstacle.worldX == character1.worldX) {
-                        character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{1}).build(), Void.class);
-                        movedRight = true;
-                        System.out.println("   Schimbare pe banda dreaptă");
-                    }
+                    throw new NullPointerException("PlayerPivot not found");
                 }
             }
 
-            // Bucla suplimentară pentru a verifica trecerea obstacolului
-            startTime = System.currentTimeMillis();
+            // avoiding obstacles
+            if (obstacle.name.contains("ObstacleHighBarrier")) {
+                character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "Slide", "Assembly-CSharp", new Object[]{}).build(), Void.class);
+
+            } else if (obstacle.name.contains("ObstacleLowBarrier") || obstacle.name.contains("Rat")) {
+                character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "Jump", "Assembly-CSharp", new Object[]{}).build(), Void.class);
+
+            } else {
+                if (allObstacles.size() > 1 && obstacle.worldZ == allObstacles.get(1).worldZ) {
+                     if (obstacle.worldX == character1.worldX) {
+                        if (allObstacles.get(1).worldX == -1.5f) {
+                            character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{1}).build(), Void.class);
+                            movedRight = true;
+                         } else {
+                            character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{-1}).build(), Void.class);
+                            movedLeft = true;
+                         }
+                    } else {
+                        if (allObstacles.get(1).worldX == character1.worldX) {
+                             if (obstacle.worldX == -1.5f) {
+                                 character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{1}).build(), Void.class);
+                                movedRight = true;
+                             } else {
+                                 character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{-1}).build(), Void.class);
+                                movedLeft = true;
+                             }
+                        }
+                    }
+                } else {
+                     if (obstacle.worldX == character1.worldX) {
+                        character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{1}).build(), Void.class);
+                        movedRight = true;
+                     }
+                }
+            }
+
+             startTime = System.currentTimeMillis();
             while (character1.worldZ - 3 < obstacle.worldZ && character1.worldX < 99 && (System.currentTimeMillis() - startTime) < 15000) {
                 params = new AltFindObjectsParams.Builder(AltDriver.By.ID, "" + obstacle.id).build();
                 obstacle = getDriver().findObject(params);
@@ -179,38 +144,61 @@ public class GamePlayPage extends BasePage {
                 params = new AltFindObjectsParams.Builder(AltDriver.By.NAME, "PlayerPivot").build();
                 character1 = getDriver().findObject(params);
                 if (character1 == null) {
-                    throw new NullPointerException("PlayerPivot nu a fost găsit.");
+                    throw new NullPointerException("PlayerPivot not found");
                 }
-                System.out.println("   Verificare trecere obstacol: Character z:" + character1.worldZ + ", Obstacle z:" + obstacle.worldZ);
-            }
+             }
 
-            // Verificăm dacă pisica s-a mutat la dreapta și trebuie să revină pe banda stângă
-            if (movedRight) {
+             if (movedRight) {
                 character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{-1}).build(), Void.class);
                 movedRight = false;
-                System.out.println("    Revenire pe banda stângă după mutare la dreapta");
-            }
+             }
             if (movedLeft) {
                 character1.callComponentMethod(new AltCallComponentMethodParams.Builder("CharacterInputController", "ChangeLane", "Assembly-CSharp", new Object[]{1}).build(), Void.class);
                 movedLeft = false;
-                System.out.println("    Revenire pe banda dreaptă după mutare la stânga");
-            }
+             }
         }
     }
+
+
     public List<AltObject> findAllFish() throws Exception {
-        // Căutăm toate obiectele de tip Fishbone
         AltFindObjectsParams params = new AltFindObjectsParams.Builder(AltDriver.By.NAME, "Fishbone").build();
         List<AltObject> allFishbones = new ArrayList<>(Arrays.asList(getDriver().findObjectsWhichContain(params)));
-        System.out.println("Toți peștii sunt în număr de " + allFishbones.size());
 
-        // Sortare folosind expresii lambda
-        allFishbones.sort((x, y) -> {
-            if (x.worldZ == y.worldZ) return 0;
-            return x.worldZ > y.worldZ ? 1 : -1;
+        allFishbones.sort((x, y) -> { // sort the list based on the elements' worldZ positions in ascending order
+            int xZ = (int) x.worldZ;
+            int yZ = (int) y.worldZ;
+            return Integer.compare(xZ, yZ);
         });
 
-        // Returnăm lista de pești
-        return allFishbones;
+        Set<String> uniqueFishPairs = new HashSet<>(); //ensures that each fish added to the final list is unique, based on a combination of the fish's ID and its adjusted worldZ value
+
+        List<AltObject> middleLaneFishbones = new ArrayList<>(); //stores the filtered list of fish that are in the middle lane.
+        int lastFishZ = 0;
+        int fishZOffset = 0;
+        int fishResetCounter = 0;
+
+        for (AltObject fish : allFishbones) {
+            if (fish.worldX == 0.0f) { // add only the middle lane fishes
+                int currentFishZ = (int) fish.worldZ;
+
+                // Detect origin reset for the fish by checking if current Z is smaller than the last Z
+                if (currentFishZ < lastFishZ) {
+                    fishResetCounter++;
+                    fishZOffset = fishResetCounter * 100; // Adjust offset by 100 for each reset
+                }
+                lastFishZ = currentFishZ;
+
+                int adjustedFishZ = currentFishZ + fishZOffset;
+                fish.worldZ = adjustedFishZ; // Update fish Z to the adjusted value
+
+                String fishPair = fish.getId() + "-" + adjustedFishZ;
+                if (uniqueFishPairs.add(fishPair)) { // Add only unique fishbones based on (ID, Z) pair
+                    middleLaneFishbones.add(fish);
+                }
+            }
+        }
+
+        return middleLaneFishbones;
     }
 
     public int getCollectedCoinsNumber() throws Exception {
@@ -227,10 +215,6 @@ public class GamePlayPage extends BasePage {
             throw new NullPointerException("CoinText object not found");
         }
 
-        System.out.println("CoinText object found name: " + coinsUI.getName());
-        System.out.println("CoinText object found id: " + coinsUI.getId());
-        System.out.println("CoinText object found text: " + coinsUI.getText());
-
         return Integer.parseInt(coinsUI.getText());
     }
     public int getDistanceRun() throws Exception {
@@ -243,14 +227,8 @@ public class GamePlayPage extends BasePage {
         AltWaitForObjectsParams params = new AltWaitForObjectsParams.Builder(par).withTimeout(10).build();
         AltObject distance = getDriver().waitForObject(params);
 
-        /*System.out.println("DistanceText object found name: " + distance.getName());
-        System.out.println("DistanceText object found id: " + distance.getId());
-        System.out.println("DistanceText object found text: " + distance.getText());*/
-
-        // Eliminăm toate caracterele non-numerice din text
         String numericText = distance.getText().replaceAll("[^\\d]", "");
 
-        // Conversia textului numeric într-un număr întreg
         try {
             return Integer.parseInt(numericText);
         } catch (NumberFormatException e) {
